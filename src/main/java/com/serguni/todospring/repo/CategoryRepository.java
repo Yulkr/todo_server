407147ee7fa9bfe00ecdc29afabfa49ba96bfbb7
Yulkr <61617785+Yulkr@users.noreply.github.com>
2021-05-18 19:07:32 +0300

    Delete src/main/java/com/serguni/todospring directory

--- src/main/java/com/serguni/todospring/repo/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.serguni.todospring.repo;
-
-import com.serguni.todospring.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- src/main/java/com/serguni/todospring/repo/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.serguni.todospring.repo;
-
-import com.serguni.todospring.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
